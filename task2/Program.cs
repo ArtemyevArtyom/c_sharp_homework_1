160def235482879105a44db5f93afbc24376bc35
ArtemyevArtyom <gof.ru@yandex.ru>
2022-09-20 16:40:21 +0400

    программа, которая на вход принимает два числа и  выдаёт, какое большее, а какое меньшее

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+ Напишите программу, которая на вход принимает два числа и 
+ выдаёт, какое большее, а какое меньшее.
+*/
+Console.Write("Введите число A: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+if (NumberA > NumberB) Console.WriteLine($"Число {NumberA} больше, чем число {NumberB}");
+else if (NumberA < NumberB) Console.WriteLine($"Число {NumberB} больше, чем число {NumberA}");
