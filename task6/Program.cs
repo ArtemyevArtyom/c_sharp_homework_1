7585760483af8219cd53fdd07ee92ce9fef240fd
ArtemyevArtyom <gof.ru@yandex.ru>
2022-09-20 19:34:01 +0400

    Программа, которая на вход принимает число и  выдаёт, является ли оно чётным

---
+++ task6/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+ Напишите программу, которая на вход принимает число и 
+ выдаёт, является ли оно чётным (делится ли оно на два без остатка).
+*/
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0 )  Console.WriteLine($"Число {Number} - чётное");
+else Console.WriteLine($"Число {Number} - нечётное");
